1e1a52749869f3a8158473ea3f55e4fa7f6c257a
deividReisSouza <vznwdwt9yy@privaterelay.appleid.com>
2026-09-02 20:13:22 -0300

    commit de soma e subtrai

--- curso-react/src/pages/Auth.jsx
+++ curso-react/src/pages/Auth.jsx
@@ -1,24 +1,35 @@
+import { useState } from "react";
 import { Link } from "react-router";
 //import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap');
 
 function Auth() {
+
+    const [doritos, setDoritos] = useState(0);
+
+    function sub(){
+        setDoritos(doritos - 1)
+    }
+
     return (
-        <div classNameName="h-full flex">
-            <div className="fixed top-0 right-0 bottom-0 left-0 bg-secundary bg-black-50 items-center justify-center shadow-md column flex text-white ">
-                <div className="relative max-w-md w-full p-5 bg-primary rounded-lg shadow-md flex column">
-                    <Link to="/" className="mb-5 text-white">Voltar</Link>
+        <div className="h-full flex intems-center min-h-screen">
+            <div className="w-1/6 mx-auto my-auto p-5 bg-primary rounded-lg shadow-md flex flex-col">
 
-                    <form className="flex flex-col text-white gap=[20]">
-                        Email:<input id="cMailLogin" type="email" placeholder="@gmail.com" />
+                <Link to="/" className="mb-5 text-white text-center rounded-md">Voltar</Link>
+                <div className="bg-red-500 mx-a" onClick={sub}>-</div>
+                {doritos}
+                <div className="bg-green-100 rounded-full p-2"onClick={() => setDoritos(doritos +1)}>+</div>
 
-                        Senha:<input id="cPassLogin" type="password" placeholder="senhA1@" />
+                <form className="flex text-white gap-[20px] text-center flex-col">
+                    Email:<input id="cMailLogin" type="email" placeholder="@gmail.com" />
 
-                        <Link id="formLogin" className=" cursor-pointer mt-5 bg-buttom text-white text-center rounded-md py-2">Entrar</Link>
-                    </form>
-                    <p id="rUserIncorrect"></p>
-                </div>
+                    Senha:<input id="cPassLogin" type="password" placeholder="senhA1@" />
+
+                    <Link id="formLogin" className=" cursor-pointer mt-5 bg-buttom text-white text-center rounded-md py-2">Entrar</Link>
+                </form>
+                <p id="rUserIncorrect"></p>
             </div>
         </div>
+
     )
 }
 
